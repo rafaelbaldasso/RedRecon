58cdbbfa955eb2a07e31c84bee4f3e0831f96bed
Rafael Baldasso <77108325+rafaelbaldasso@users.noreply.github.com>
2023-10-20 09:18:34 -0300

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]
+    then
+        echo;echo "[!] You must run the setup as root!";echo
+        exit
+fi
+
+echo;echo "-> Installing dependencies and starting services...";echo
+
+sudo apt install nmap dirb sslscan dnsutils sendemail postfix golang subfinder -y
+
+go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
+
+sudo systemctl enable postfix && sudo service postfix start
+
+#wget https://raw.githubusercontent.com/santoru/shcheck/master/shcheck.py
+#wget https://raw.githubusercontent.com/Ogglas/Orignal-Slowloris-HTTP-DoS/master/slowloris.pl
+
+chmod +x shcheck.py slowloris.pl
+
+python3 -m pip install --upgrade pip
+python3 -m pip install --upgrade requests
+python3 -m pip install ultimate_sitemap_parser
+
+echo;echo "-> Done!";echo
